52874debaf57c13737d4f82c3bfcb8ab0d441941
danieltatarkin <danieltatarkin@gmail.com>
2018-04-01 10:49:20 -0700

    All

--- app/src/main/java/com/max/explo/bibo/ContactActivity.java
+++ app/src/main/java/com/max/explo/bibo/ContactActivity.java
@@ -1,24 +1,18 @@
 package com.max.explo.bibo;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Parcelable;
+import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.*;
 
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.max.explo.bibo.model.ContactCard;
+
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -30,6 +24,9 @@ public class ContactActivity extends AppCompatActivity {
     private Button saveButton;
     private boolean editing;
     private int CARD_NUMBER;
+    private boolean emailRead;
+    private boolean phoneRead;
+    private boolean addButtonFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +39,18 @@ public class ContactActivity extends AppCompatActivity {
         companyName = findViewById(R.id.companyNameText);
         exportButton = findViewById(R.id.exportButton);
         saveButton = findViewById(R.id.saveButton);
+        Button addButton = findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OcrCaptureActivity.class);
+                intent.putExtra(OcrCaptureActivity.AutoFocus, true);
+                intent.putExtra(OcrCaptureActivity.UseFlash, false);
+
+                startActivityForResult(intent, MainActivity.RC_OCR_CAPTURE);
+            }
+        });
 
         Intent data = getIntent();
         email.setText(data.getStringExtra("EMAIL"));
@@ -49,21 +58,21 @@ public class ContactActivity extends AppCompatActivity {
         name.setText(data.getStringExtra("NAME"));
 
         if(data.getAction() == MainActivity.ACTION_POPULATE) {
-            int CARD_NUMBER = data.getIntExtra("CARD_NUMBER",-1);
+            CARD_NUMBER = data.getIntExtra("CARD_NUMBER",-1);
             switch (CARD_NUMBER){
-                case 1:
+                case 0:
                     populateCard((ContactCard) data.getParcelableExtra("CARD1_CARD"));
                     break;
-                case 2:
+                case 1:
                     populateCard((ContactCard) data.getParcelableExtra("CARD2_CARD"));
                     break;
-                case 3:
+                case 2:
                     populateCard((ContactCard) data.getParcelableExtra("CARD3_CARD"));
                     break;
-                case 4:
+                case 3:
                     populateCard((ContactCard) data.getParcelableExtra("CARD4_CARD"));
                     break;
-                case 5:
+                case 4:
                     populateCard((ContactCard) data.getParcelableExtra("CARD5_CARD"));
                     break;
             }
@@ -115,5 +124,59 @@ public class ContactActivity extends AppCompatActivity {
         email.setText(contactCard.getEmail());
         companyName.setText(contactCard.getCompanyName());
         editing = true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == MainActivity.RC_OCR_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    String text = data.getStringExtra(OcrCaptureActivity.TextBlockObject);
+                    getEmail(text);
+                    if (emailRead) email.setText(getEmail(text));
+                    getPhone(text);
+                    if (phoneRead) phone.setText(getPhone(text));
+                    if (!emailRead && !phoneRead) name.setText(getName(text));
+                    editing = true;
+                }
+            }
+        }
+    }
+
+    private String getEmail(String text) {
+        String[] ctext = text.split(" |\n");
+        String cemail = "";
+        for (String s : ctext) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '@') {
+                    cemail = s;
+                    emailRead = true;
+                    return cemail;
+                }
+            }
+        }
+        return "";
+    }
+
+    private String getPhone(String text) {
+        String[] ctext = text.split("\n");
+        for (String s : ctext) {
+            String cnumb = "";
+            for (int i = 0; i < s.length(); i++) {
+                if (!(s.charAt(i) < '0' || s.charAt(i) > '9')) {
+                    cnumb += s.charAt(i);
+                }
+            }
+            if (cnumb.length() == 10 || cnumb.length() == 11) {
+                phoneRead = true;
+                return cnumb;
+            }
+        }
+        return "";
+    }
+
+    private String getName(String text) {
+        String[] ctext = text.split("\n");
+        return ctext[0];
     }
 }
--- app/src/main/java/com/max/explo/bibo/MainActivity.java
+++ app/src/main/java/com/max/explo/bibo/MainActivity.java
@@ -1,25 +1,40 @@
 package com.max.explo.bibo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Parcelable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.max.explo.bibo.model.ContactCard;
 
-import java.lang.reflect.Array;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private EditText mEditTexto;
+    private EditText mEditTextSubject;
+    private EditText mEditTextMessage;
+
     public static final String CARD_KEY = "CARD_KEY";
     public static final String ACTION_POPULATE = "ACTION_POPULATE";
 
+
+    private boolean callOnly = false;
+    private boolean mailOnly = false;
     private boolean autoFocus = true;
     private boolean useFlash = false;
     private Button cameraButton;
@@ -33,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
     ContactCard[] cardList = new ContactCard[5];
 
 
-    private static final int RC_OCR_CAPTURE = 9003;
+    public static final int RC_OCR_CAPTURE = 9003;
     private static final String TAG = "MainActivity";
     private boolean emailRead = false;
     private boolean phoneRead = false;
@@ -47,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         cameraButton = findViewById(R.id.cameraB);
+        Button emailButton = findViewById(R.id.mailB);
+        Button phoneButton = findViewById(R.id.phoneB);
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,40 +82,76 @@ public class MainActivity extends AppCompatActivity {
         card4 = findViewById(R.id.card4);
         card5 = findViewById(R.id.card5);
 
+
         card1.setOnClickListener(cardHandler);
         card2.setOnClickListener(cardHandler);
         card3.setOnClickListener(cardHandler);
         card4.setOnClickListener(cardHandler);
         card5.setOnClickListener(cardHandler);
 
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OcrCaptureActivity.class);
+                intent.putExtra(OcrCaptureActivity.AutoFocus, autoFocus);
+                intent.putExtra(OcrCaptureActivity.UseFlash, useFlash);
+                mailOnly = true;
+                startActivityForResult(intent, RC_OCR_CAPTURE);
+            }
+        });
+
+        phoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OcrCaptureActivity.class);
+                intent.putExtra(OcrCaptureActivity.AutoFocus, autoFocus);
+                intent.putExtra(OcrCaptureActivity.UseFlash, useFlash);
+                callOnly = true;
+                startActivityForResult(intent, RC_OCR_CAPTURE);
+            }
+        });
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == RC_OCR_CAPTURE) {
+        if (requestCode == RC_OCR_CAPTURE) {
             if (resultCode == CommonStatusCodes.SUCCESS) {
                 if (data != null) {
                     String text = data.getStringExtra(OcrCaptureActivity.TextBlockObject);
                     Log.d(TAG, "Text read: " + text);
                     email = getEmail(text);
                     phone = getPhone(text);
-                    Intent intent = new Intent(getApplicationContext(), ContactActivity.class);
-                    if (emailRead) {
-                        debugToast("Email", email);
-                        intent.putExtra("EMAIL",email);
-                    } if (phoneRead) {
-                        debugToast("Phone", phone);
-                        intent.putExtra("PHONE",phone);
-                    }
-
-                    if (!emailRead && !phoneRead){
-//                        debugToast("ERROR", "nothing found");
-                        intent.putExtra("NAME",getName(text));
+                    if(mailOnly){
+                        if(emailRead) {
+                            prepMail();
+                        }
+                        mailOnly = false;
+                    } else if (callOnly){
+                        if(phoneRead){
+                            directCall();
+                        }
+                        callOnly = false;
                     } else {
+                        Intent intent = new Intent(getApplicationContext(), ContactActivity.class);
+                        if (emailRead) {
+                            debugToast("Email", email);
+                            intent.putExtra("EMAIL", email);
+                        }
+                        if (phoneRead) {
+                            debugToast("Phone", phone);
+                            intent.putExtra("PHONE", phone);
+                        }
+
+                        if (!emailRead && !phoneRead) {
+//                        debugToast("ERROR", "nothing found");
+                            intent.putExtra("NAME", getName(text));
+                        } else {
 //                        debugToast("ERROR", "here");
-                    }
+                        }
 //                    startActivityForResult(intent,requestCode);
-                    trySaving(getName(text), email, phone, intent);
+                        trySaving(getName(text), email, phone, intent);
+                    }
                     phoneRead = false;
                     emailRead = false;
 
@@ -107,8 +160,15 @@ public class MainActivity extends AppCompatActivity {
                 }
             } else {
             }
-        } else if (requestCode == 6001) {
+        } else if (requestCode == 6003 && resultCode == RESULT_OK) {
             Parcelable parcelable = data.getParcelableExtra("CONTACT_CARD");
+            boolean temp = false;
+            int tempNumber =0;
+            if (data.getIntExtra("CARD_NUMBER",-1) > -1){
+                tempNumber = cardIndex;
+                cardIndex = data.getIntExtra("CARD_NUMBER",0);
+                temp = true;
+            }
             cardList[cardIndex] = (ContactCard) parcelable;
             switch (cardIndex) {
                 case 0:
@@ -127,20 +187,20 @@ public class MainActivity extends AppCompatActivity {
                     card5.setText(cardList[cardIndex].getName());
                     break;
             }
-            cardIndex++;
-            if (cardIndex == 5) cardIndex = 0;
-        }
-        else {
+            if (!temp) cardIndex++;
+            if (temp) cardIndex = tempNumber;
+            if (cardIndex == 5) cardIndex = 4;
+        } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
     private void debugToast(String action, String text) {
-        Toast.makeText(this, action +" is " + text, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, action + " is " + text, Toast.LENGTH_LONG).show();
     }
 
-    private void trySaving(String name, String email, String phone, Intent intent){
-        startActivityForResult(intent,6001);
+    public void trySaving(String name, String email, String phone, Intent intent) {
+        startActivityForResult(intent, 6003);
     }
 
     public View.OnClickListener cardHandler = new View.OnClickListener() {
@@ -150,43 +210,43 @@ public class MainActivity extends AppCompatActivity {
 
             switch (v.getId()) {
                 case R.id.card1:
-                    if (card1.getText() != ""){
+                    if (card1.getText() != "") {
                         intent.setAction(ACTION_POPULATE);
-                        intent.putExtra("CARD_NUMBER",1);
-                        intent.putExtra("CARD1_CARD",cardList[0]);
-                        startActivity(intent);
+                        intent.putExtra("CARD_NUMBER", 0);
+                        intent.putExtra("CARD1_CARD", cardList[0]);
+                        startActivityForResult(intent, 6003);
                     }
                     break;
                 case R.id.card2:
-                    if (card2.getText() != ""){
+                    if (card2.getText() != "") {
                         intent.setAction(ACTION_POPULATE);
-                        intent.putExtra("CARD_NUMBER",2);
-                        intent.putExtra("CARD2_CARD",cardList[1]);
-                        startActivity(intent);
+                        intent.putExtra("CARD_NUMBER", 1);
+                        intent.putExtra("CARD2_CARD", cardList[1]);
+                        startActivityForResult(intent, 6003);
                     }
                     break;
                 case R.id.card3:
-                    if (card3.getText() != ""){
+                    if (card3.getText() != "") {
                         intent.setAction(ACTION_POPULATE);
-                        intent.putExtra("CARD_NUMBER",3);
-                        intent.putExtra("CARD3_CARD",cardList[2]);
-                        startActivity(intent);
+                        intent.putExtra("CARD_NUMBER", 2);
+                        intent.putExtra("CARD3_CARD", cardList[2]);
+                        startActivityForResult(intent, 6003);
                     }
                     break;
                 case R.id.card4:
-                    if (card4.getText() != ""){
+                    if (card4.getText() != "") {
                         intent.setAction(ACTION_POPULATE);
-                        intent.putExtra("CARD_NUMBER",4);
-                        intent.putExtra("CARD4_CARD",cardList[3]);
-                        startActivity(intent);
+                        intent.putExtra("CARD_NUMBER", 3);
+                        intent.putExtra("CARD4_CARD", cardList[3]);
+                        startActivityForResult(intent, 6003);
                     }
                     break;
                 case R.id.card5:
-                    if (card5.getText() != ""){
+                    if (card5.getText() != "") {
                         intent.setAction(ACTION_POPULATE);
-                        intent.putExtra("CARD_NUMBER",5);
-                        intent.putExtra("CARD5_CARD",cardList[4]);
-                        startActivity(intent);
+                        intent.putExtra("CARD_NUMBER", 4);
+                        intent.putExtra("CARD5_CARD", cardList[4]);
+                        startActivityForResult(intent, 6003);
                     }
                     break;
             }
@@ -198,8 +258,8 @@ public class MainActivity extends AppCompatActivity {
         String[] ctext = text.split(" |\n");
         String cemail = "";
         for (String s : ctext) {
-            for(int i = 0; i < s.length(); i++){
-                if(s.charAt(i) == '@'){
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '@') {
                     cemail = s;
                     emailRead = true;
                     return cemail;
@@ -209,18 +269,12 @@ public class MainActivity extends AppCompatActivity {
         return "";
     }
 
-    private void phoneSaved(String action, String text) {
-        String tphone = getPhone(text);
-        if (tphone != "") { Toast.makeText(this, action +" is " + tphone, Toast.LENGTH_SHORT).show(); }
-        phoneRead = false;
-    }
-
     private String getPhone(String text) {
         String[] ctext = text.split("\n");
         for (String s : ctext) {
             String cnumb = "";
-            for (int i = 0; i < s.length(); i++){
-                if(!(s.charAt(i) < '0' || s.charAt(i) > '9')){
+            for (int i = 0; i < s.length(); i++) {
+                if (!(s.charAt(i) < '0' || s.charAt(i) > '9')) {
                     cnumb += s.charAt(i);
                 }
             }
@@ -237,13 +291,54 @@ public class MainActivity extends AppCompatActivity {
         return ctext[0];
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
+    protected void directCall() {
+        final int REQUEST_PHONE_CALL = 1;
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + phone));
+        //checks for permission before placing the call
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_PHONE_CALL);
+            } else {
+                //places call
+                startActivity(callIntent); //note have to attempt to call twice on the time running on a phone
+            }
+        }
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    protected void prepMail() {
+        setContentView(R.layout.email_layout);
+
+        mEditTexto = findViewById(R.id.edit_text_to);
+        mEditTexto.setText(email);
+        mEditTextSubject = findViewById(R.id.edit_text_subject);
+        mEditTextMessage = findViewById(R.id.edit_text_message);
+
+
+        Button buttonSend = findViewById(R.id.button_send);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+
+            }
+        });
+    }
+
+    private void sendMail() {
+        String[] recipient = new String[]{mEditTexto.getText().toString()};
+        //String[] recipients = recipientList.split(",");
+        //example1@gmail.com, example2@gmail.com
+
+        String subject = mEditTextSubject.getText().toString();
+        String message = mEditTextMessage.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipient);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
     }
 }
